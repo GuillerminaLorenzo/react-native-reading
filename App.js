6fc74e7cee28da0c6c8ca73eda86a704aa66e83d
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-02-13 14:35:56 +0000

    Refactored App, created WebViewComponent

--- App.js
+++ App.js
@@ -1,39 +1,22 @@
-import React, { useRef, useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { View } from 'react-native';
-import { WebView } from 'react-native-webview';
 import styles from "./scr/components/style/Styles";
 import NextButton from './scr/components/buttons/NextButton';
-import PrevButton from './scr/components/buttons/PrevButton'
+import PrevButton from './scr/components/buttons/PrevButton';
+import WebViewComponent from './scr/components/webview/WebViewComponent';
 
 const App = () => {
-
   const webviewRef = useRef();
 
   const [currentHeading, setCurrentHeading] = useState(0);
   const [pageHeading, setPageHeading] = useState(0);
 
-  const onMessage = (event) => {
-    const data = event.nativeEvent.data;
-    setPageHeading(JSON.parse(data.split(',')))
-  };
-
-  const injectedjs = `const headings = JSON.stringify(Array.from(document.querySelectorAll('h1, h2, h3, h4, h5, h6')).map(heading => ({
-      top: heading.offsetTop
-    })));
-    window.ReactNativeWebView.postMessage(headings);
-  `
-
   return (
     <View style = {styles.container}>
-      <WebView
-        style = {styles.webview}
-        allowFileAccess={true}
-        originWhitelist={['*']}
+      <WebViewComponent
         source={{html: require('./assets/index')()}}
-        ref = {webviewRef}
-        onMessage={onMessage}
-        javaScriptEnabled={true}
-        injectedJavaScript={injectedjs}
+        webviewRef={webviewRef}
+        setPageHeading={setPageHeading}
       />
       <View style = {styles.touchableOpacityContainer}>
       <PrevButton 
